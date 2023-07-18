0672c0a53f9151185d7d371398a14ce914e7fe7e
Zakaria Bakour <bangorzakaria@gmail.com>
2023-07-18 09:02:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+
+	int j;
+	
+	for (j = 0; j < 10; j++)
+	{
+		int i;
+		for (i = 'a'; i < '{'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}	
