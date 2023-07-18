9f2254d09d1538e80834286de88988fb2c5f296d
Zakaria Bakour <bangorzakaria@gmail.com>
2023-07-18 09:09:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet
  *
- * Description: prints 10 times the alphabet, in lowercase, followed by a new line.
+ * Description: prints 10 times the alphabet, lowercase
  *
  * Return: Always 0 (Success)
  */
