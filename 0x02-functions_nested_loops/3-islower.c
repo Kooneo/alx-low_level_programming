e58c71db48936780dbcfe084078ee727362b7b07
Zakaria Bakour <bangorzakaria@gmail.com>
2023-07-18 09:26:14 -0700

    wip

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -  checks for lowercase character
+ *
+ * Description: Returns 1 if c is lowercase otheewise 0
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c);
+{
+	if (islower(c))
+	{
+		return 1;
+	}
+
+	return 0;
+}
