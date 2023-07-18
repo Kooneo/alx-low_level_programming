b75bc56e5f7d985be90675cd045dc38a3c7093b7
Zakaria Bakour <bangorzakaria@gmail.com>
2023-07-18 10:00:44 -0700

    wip

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign -  prints the sign of a number
- * @c: first parameter
+ * @n: first parameter
  * Description: function that prints the sign of a number.
  *
  * Return: 1 or 0 or -1
