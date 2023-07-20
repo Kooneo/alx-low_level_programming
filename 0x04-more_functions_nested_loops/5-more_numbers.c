3cfd1b3f765d6300a06ce9d11c485b1c9f564fe0
Kooneo <husa.bakour02@gmail.com>
2023-07-20 21:16:03 +0100

    wip

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+    int j;
+
+    for (j =0; j <= 10; j++)
+    {
+        int i;
+
+        for (i = 0; i <= 14; i++)
+        {
+            _putchar(i + '0');
+        }
+
+        _putchar('\n');
+    }
+    
+}
