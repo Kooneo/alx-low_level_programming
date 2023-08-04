49146438f4226c2dac5cec8aee4fe21ffb351b0c
Zakaria Bakour <bangorzakaria@gmail.com>
2023-08-04 12:19:10 -0700

    work in progress

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int total;
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		total = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", total);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
